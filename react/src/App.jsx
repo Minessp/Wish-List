071059cfc6518c83de3b545e62f5c6c7aefdae05
Minessp <minestadionchannel@gmail.com>
2025-05-07 13:22:12 -0300

    Adicionado estrutura básica do react + tailwind

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
 
 function App() {
-
+    return(
+        <div className="w-screen h-screen bg-linear-to-r from-bg-start
+         to-bg-end to-25% flex justify-center p-6 font-outfit">
+            <h1 className="text-[2.5rem] font-[700] text-[#ffffff] text-shadow-home-title">
+                Minhas Listas de Desejos
+            </h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
