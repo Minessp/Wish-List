fec41780307e28a11f58d847b1325f680622a43f
Minessp <minestadionchannel@gmail.com>
2025-05-19 16:07:50 -0300

    Adicionado responsividade para a página de cadastro

--- react/src/pages/Signup.jsx
+++ react/src/pages/Signup.jsx
@@ -3,23 +3,24 @@ import Logo from "../assets/logo/logo.png";
 export const Signup = () => {
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-bg-start to-bg-end to-25% flex flex-col">
-            <header className="flex items-center px-6 py-4 fixed w-full z-20 bg-transparent">
+            <header className="flex items-center px-6 py-4 sm:fixed w-full z-20">
                 <a href="/">
                     <img className="h-8" src={Logo} alt="Logo" />
                 </a>
             </header>
 
-            <div className="flex flex-grow">
-                <div className="flex-1 flex items-center justify-center px-4">
-                    <h1 className="text-[2.5rem] font-semibold font-outfit text-white"
+            <div className="flex flex-grow flex-col sm:flex-row">
+                <div className="flex-1 flex items-center justify-center px-4 py-10 md:py-0">
+                    <h1 className="text-[2.5rem] font-semibold font-outfit text-white text-center"
                         style={{ textShadow: "2px 2px 10px rgba(0,0,0,0.5)" }}
                     >
                         Todos seus desejos em um só lugar
                     </h1>
                 </div>
 
-                <div className="w-[30%] relative">
-                    <form className="absolute inset-0 bg-white p-10 rounded-l-3xl shadow-2xl flex flex-col justify-center">
+                <div className="w-full sm:w-[40%] lg:w-[30%] relative">
+                    <form className="absolute sm:h-full inset-0 bg-white p-10 max-sm:rounded-3xl sm:rounded-l-3xl sm:m-0
+                    shadow-2xl flex flex-col justify-center max-lg:relative max-lg:mx-4 max-lg:my-10">
                         <div className="w-full flex flex-col items-center">
                             <h2 className="text-4xl font-bold font-outfit mb-8 text-center text-shadow-md">Cadastro</h2>
 
@@ -53,7 +54,7 @@ export const Signup = () => {
 
                             <div className="mt-4 text-center">
                                 <span className="text-sm font-semibold">Já tem uma conta? </span>
-                                <a href="/login" className="text-sm text-blue-600 hover:underline font-semibold font-outfit">
+                                <a href="#" className="text-sm text-blue-600 hover:underline font-semibold font-outfit">
                                     Entrar
                                 </a>
                             </div>
